61b9d62713091cc42828989ae7dd0b2e3c9c9511
platosha-git <15platosha@gmail.com>
2022-04-23 03:25:51 +0300

    done lab1

--- lab1/neurons.py
+++ lab1/neurons.py
@@ -1,37 +1,79 @@
 import random
+import numpy as np
 
-
-class Neuron:
-    def __init__(self, f_initialize, f_activate):
-        self.initialize = f_initialize
-        self.activate = f_activate
-
-    def solve(self, inputs):
-        raise NotImplementedError
-
-    def correct(self, expected_result):
-        pass
-
-
-class SNeuron(Neuron):
-    def solve(self, inputs):
-        return self.activate(inputs)
 
 def init_s_neurons(size):
     neurons = []
     for i in range(size):
-        neuron = SNeuron(None, lambda value: value)
+        neuron = SNeuron(lambda value: value)
+        neurons.append(neuron)
+    return neurons
+
+def init_a_neurons(size, s_size):
+    neurons = []
+    for i in range(size):
+        neuron = ANeuron(lambda value: int(value > 0), s_size)
+        neurons.append(neuron)
+    return neurons
+
+def init_r_neurons(size, a_size, learning_speed):
+    neurons = []
+    for i in range(size):
+        neuron = RNeuron(lambda: 0, lambda value: int(value >= 0), a_size, learning_speed)
         neurons.append(neuron)
     return neurons
 
 
-class ActivationNeuron(Neuron):
-    def __init__(self, f_initialize, f_activate, inputs_count):
-        super().__init__(f_initialize, f_activate)
+
+class SNeuron():
+    def __init__(self, f_activ):
+        self.activate = f_activ
+
+    def solve(self, inputs):
+        return self.activate(inputs)
+
+
+class ANeuron():
+    def __init__(self, f_activate, inputs_count):
+        self.activate = f_activate
         self.input_weights = self.get_weights(inputs_count)
         self.bias = self.get_bias()
         self.last_inputs = None
         self.last_result = None
+
+    def accumulate(self, inputs):
+        accumulation = -self.bias
+        for value, weight in zip(inputs, self.input_weights):
+            accumulation += value * weight
+        return accumulation
+
+    def solve(self, inputs):
+        self.last_inputs = inputs
+        self.last_result = self.activate(self.accumulate(inputs))
+        return self.last_result
+
+    def get_bias(self):
+        bias = 0
+        for weight in self.input_weights:
+            if weight > 0:
+                bias += 1
+            if weight < 0:
+                bias -= 1
+        return bias
+
+    def get_weights(self, count):
+        return [random.choice([-1, 0, 1]) for i in range(count)]
+
+
+class RNeuron():
+    def __init__(self, f_initialize, f_activate, inputs_count, learning_speed, bias=0):
+        self.initialize = f_initialize
+        self.activate = f_activate
+        self.input_weights = self.get_weights(inputs_count)
+        self.last_inputs = None
+        self.last_result = None
+        self.learning_speed = learning_speed
+        self.bias = bias
 
     def accumulate(self, inputs):
         accumulation = - self.bias
@@ -47,41 +89,8 @@ class ActivationNeuron(Neuron):
     def get_weights(self, count):
         return [self.initialize() for _ in range(count)]
 
-    def get_bias(self):
-        return self.initialize()
-
-
-class ANeuron(ActivationNeuron):
-    def get_bias(self):
-        bias = 0
-        for weight in self.input_weights:
-            if weight > 0:
-                bias += 1
-            if weight < 0:
-                bias -= 1
-        return bias
-
-    def get_weights(self, count):
-        return [random.choice([-1, 0, 1]) for _ in range(count)]
-
-def init_a_neurons(size, s_size):
-    neurons = []
-    for i in range(size):
-        neuron = ANeuron(None, lambda value: int(value >= 0), s_size)
-        neurons.append(neuron)
-    return neurons
-
-
-class RNeuron(ActivationNeuron):
-    def __init__(self, f_initialize, f_activate, inputs_count, learning_speed, bias):
-        super().__init__(f_initialize, f_activate, inputs_count)
-        self.learning_speed = learning_speed
-        self.bias = bias
-
-    # корректировка весов при ошибке
     def correct(self, expected_result):
         if expected_result != self.last_result:
-            # градиентный спуск
             lr = 1
             if self.last_result == 0:
                 lr = -1
@@ -92,11 +101,4 @@ class RNeuron(ActivationNeuron):
                 for input_weight, delta_weight in zip(self.input_weights, delta_weight)
             ]
             self.bias += lr * self.learning_speed
-
-def init_r_neurons(size, a_size, learning_speed):
-    neurons = []
-    for i in range(size):
-        neuron = RNeuron(lambda: 0, lambda value: 1 if value >= 0 else 0, a_size, learning_speed, bias=0)
-        neurons.append(neuron)
-    return neurons
             
